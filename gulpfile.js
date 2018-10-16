b6523b057b9706427551d371a118bf780bc9296d
Artem Emelyanov <anemelyanov@vtb.ru>
2018-10-16 16:35:38 +0300

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
-var concatCss = require('gulp-concat-css');
-var replace = require('gulp-html-replace');
-var htmlminify = require("gulp-html-minify");
-var includer = require('gulp-htmlincluder');
-var livereload = require('gulp-livereload');
-var rename = require("gulp-rename");
-var imagemin = require('gulp-imagemin');
-var browserSync = require('browser-sync').create();
-var plumber = require('gulp-plumber');
-var concat = require('gulp-concat');
-var less = require('gulp-less');
-var uglify = require('gulp-uglify');
-var spritesmith = require("gulp-spritesmith");
-var gulpif = require("gulp-if");
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const htmlminify = require("gulp-html-minify");
+const replace = require('gulp-html-replace');
+const includer = require('gulp-htmlincluder');
+const gulpif = require("gulp-if");
+const imagemin = require('gulp-imagemin');
+const less = require('gulp-less');
+const plumber = require('gulp-plumber');
+const rename = require("gulp-rename");
+const spritesmith = require("gulp-spritesmith");
+const uglify = require('gulp-uglify');
 
 gulp.task('html', function() {
     gulp.src('dev/**/*.html')
@@ -29,57 +28,35 @@ gulp.task('html', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('less', function () {
-	gulp.src('dev/less/general.less')
-		.pipe(plumber())
-		.pipe(less())
-		.pipe(plumber.stop())
-		.pipe(gulp.dest('dev/css/imports'));
-});
-gulp.task('lessTop', function () {
+gulp.task('criticalCss', function () {
     gulp.src('dev/less/headStyle.less')
         .pipe(plumber())
         .pipe(less())
-        .pipe(plumber.stop())
-        .pipe(gulp.dest('dev/css/'));
-});
-
-gulp.task('css', function () {
-	gulp.src('dev/css/imports/*.css')
-		.pipe(plumber())
-    	.pipe(concatCss("style.css"))
-    	.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
-    	.pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(rename('style.min.css'))
-        .pipe(plumber.stop())
-    	.pipe(gulp.dest('build/css/'))
-    	.pipe(browserSync.stream());
-});
-
-gulp.task('cssTop', function () {
-	gulp.src('dev/css/headStyle.css')
-		.pipe(plumber())
-    	.pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
-    	.pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(rename('-headStyle.html'))
         .pipe(plumber.stop())
-    	.pipe(gulp.dest('dev'));
-    	// .pipe(browserSync.stream());
+        .pipe(gulp.dest('dev'));
 });
 
-gulp.task('js', function() {
-    gulp.src('dev/js/concat/*.js')
-      	.pipe(plumber())
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(rename('all.min.js'))
+gulp.task('style', function() {
+    gulp.src('dev/less/general.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename('style.min.css'))
         .pipe(plumber.stop())
-        .pipe(gulp.dest('build/js/'))
+        .pipe(gulp.dest('build/css/'))
         .pipe(browserSync.stream());
 });
+
 gulp.task('jsCore', function() {
     gulp.src('dev/js/core.js')
         .pipe(plumber())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
         .pipe(uglify())
         .pipe(rename('core.min.js'))
         .pipe(plumber.stop())
@@ -87,12 +64,26 @@ gulp.task('jsCore', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('img', () =>
+gulp.task('js', function() {
+    gulp.src('dev/js/concat/*.js')
+      	.pipe(plumber())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(rename('all.min.js'))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest('build/js/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('img', function() {
     gulp.src('dev/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('build/img'))
         .pipe(browserSync.stream())
-);
+});
 
 gulp.task('sprite', function () {
     gulp.src('dev/sprites/*.png')
@@ -132,40 +123,34 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('default', function() {
-	gulp.start('html', 'css', 'cssTop', 'less', 'lessTop', 'js', 'jsCore', 'img', 'sprite', 'lib', 'font', 'php', 'browser-sync');
+	gulp.start('criticalCss', 'html', 'style', 'js', 'jsCore', 'img', 'sprite', 'lib', 'font', 'php', 'browser-sync');
 
-	gulp.watch(['dev/**/*.html'], function(event) {
+	gulp.watch(['dev/**/*.html'], function() {
 		gulp.start('html');
 	});
-    gulp.watch(['dev/less/**/*.less'], function(event) {
-        gulp.start('less');
-        gulp.start('lessTop');
+    gulp.watch(['dev/less/**/*.less'], function() {
+        gulp.start('criticalCss');
+        gulp.start('style');
     });
-	gulp.watch(['dev/css/imports/*.css'], function(event) {
-		gulp.start('css');
-	});
-    gulp.watch(['dev/css/headStyle.css'], function(event) {
-        gulp.start('cssTop');
-    });
-	gulp.watch(['dev/js/concat/*.js'], function(event) {
+	gulp.watch(['dev/js/concat/*.js'], function() {
 		gulp.start('js');
 	});
-    gulp.watch(['dev/js/core.js'], function(event) {
+    gulp.watch(['dev/js/core.js'], function() {
         gulp.start('jsCore');
     });
-	gulp.watch(['dev/img/**/*'], function(event) {
+	gulp.watch(['dev/img/**/*'], function() {
 		gulp.start('img');
 	});
-	gulp.watch(['dev/sprites/*'], function(event) {
+	gulp.watch(['dev/sprites/*'], function() {
 		gulp.start('sprite');
 	});
-    gulp.watch(['dev/lib/**/*'], function(event) {
+    gulp.watch(['dev/lib/**/*'], function() {
         gulp.start('lib');
     });
-    gulp.watch(['dev/fonts/**/*'], function(event) {
+    gulp.watch(['dev/fonts/**/*'], function() {
         gulp.start('font');
     });
-    gulp.watch(['dev/php/**/*'], function(event) {
+    gulp.watch(['dev/php/**/*'], function() {
         gulp.start('php');
     });
 });
